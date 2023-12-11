fe5486dd6df6a0ecf517d3de77e1c5123e566b9b
Smart Dev <johnathonponce432@gmail.com>
2023-12-11 05:02:16 -0500

    use useReducer hook instead of useState

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,35 +1,46 @@
-import { useState } from "react";
+import { useState, useReducer } from 'react'
+
+function reducer(state, action) {
+  console.log(state, action)
+  if (action.type === 'inc') return state + 1
+  if (action.type === 'dec') return state - 1
+  if (action.type === 'setCount') return action.payload
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
-  const [step, setStep] = useState(1);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0)
+  const [step, setStep] = useState(1)
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
-  date.setDate(date.getDate() + count);
+  const date = new Date('june 21 2027')
+  date.setDate(date.getDate() + count)
 
   const dec = function () {
+    dispatch({ type: 'dec' })
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
-  };
+    // setCount((count) => count - step)
+  }
 
   const inc = function () {
+    dispatch({ type: 'inc' })
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
-  };
+    // setCount((count) => count + step)
+  }
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
-  };
+    dispatch({ type: 'setCount', payload: Number(e.target.value) })
+    // setCount(Number(e.target.value))
+  }
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
-  };
+    setStep(Number(e.target.value))
+  }
 
   const reset = function () {
-    setCount(0);
-    setStep(1);
-  };
+    // setCount(0)
+    setStep(1)
+  }
 
   return (
     <div className="counter">
@@ -56,6 +67,6 @@ function DateCounter() {
         <button onClick={reset}>Reset</button>
       </div>
     </div>
-  );
+  )
 }
-export default DateCounter;
+export default DateCounter
