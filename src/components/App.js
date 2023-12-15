99e4c447b8c417dfc54801efd19d07137bba26e7
Smart Dev <johnathonponce432@gmail.com>
2023-12-15 04:44:51 -0500

    create finish screen

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from './StartScreen'
 import Question from './Question'
 import NextButton from './NextButton'
 import Progress from './Progress'
+import FinishScreen from './FinishScreen'
 
 const initialState = {
   questions: [],
@@ -96,6 +97,9 @@ function App() {
             />
             <NextButton dispatch={dispatch} answer={answer} />
           </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} />
         )}
       </Main>
     </div>
